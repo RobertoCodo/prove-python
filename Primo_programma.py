c38115fb74de5f37e49f8b7bf59bc3bac54932bb
robertocodo <roberto.codognesi@gmail.com>
2025-08-14 15:58:21 +0200

    Prima versione robot Chappie

---
+++ Primo_programma.py
@@ -0,0 +1,15 @@
+# Il nostro primo programma in Python
+
+robot_name = "Chappie"
+robot_age = 1
+print("Ciao, sono " + robot_name + " e ho " + str(robot_age) + " anno.")
+
+user_name = input("Come ti chiami? ")
+print("Ciao " + user_name + ", piacere di conoscerti!")
+
+user_age = input("Quanti anni hai? ")
+age_difference = robot_age - int(user_age)
+print("La differenza di età tra noi è di " + str(age_difference) + " anni.")
+print("Grazie per aver interagito con me, " + user_name + "!")
+print("Arrivederci!")
+print("Spero di rivederti presto!")
