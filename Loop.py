17c5dbb4234c43cdb733bc60879cc418cb4cd109
robertocodo <roberto.codognesi@gmail.com>
2025-08-18 12:17:08 +0200

    Skip every 5

--- Loop.py
+++ Loop.py
@@ -1,9 +1,13 @@
 run = True
 Counter = 0
+Skip = 5
 Stop = 100000
 while run:
     Counter += 1
     print(Counter)
+    if Counter == Skip:
+        print("Loop skipped at", Counter)
+        Skip += 5
     if Counter >= Stop:
         print("Loop stopped at", Counter)
         break
